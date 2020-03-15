760fabb65e95af4620546ab58f32a6f28bc475ed
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-15 11:38:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -33,7 +33,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  
 
 
   # add 4 gun powder to the shipping_manifest hash below
